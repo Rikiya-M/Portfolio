f04e8d4d6f4c715be0aea350763292fc8bf6cb4e
Rikiya-M <121679942+Rikiya-M@users.noreply.github.com>
2023-05-05 00:05:54 +0900

    add scroll point

--- js/script.js
+++ js/script.js
@@ -24,6 +24,64 @@ $("#splash").css("display", "none");
 
 
 
+var elemTop = [];
+
+function PositionCheck(){
+
+var headerH = $("#header").outerHeight(true);
+
+	
+$(".scroll-point").each(function(i) {
+elemTop[i] =Math.round(parseInt($(this).offset().top-headerH));
+});
+}
+
+
+function ScrollAnime() {
+var scroll = Math.round($(window).scrollTop());
+var NavElem = $("#navi li");
+$("#navi li").removeClass('current');
+if(scroll >= 0 && scroll < elemTop[1]) {
+$(NavElem[0]).addClass('current');
+}
+else if(scroll >= elemTop[1] && scroll < elemTop[2]) {
+$(NavElem[1]).addClass('current');
+} 
+else if(scroll >= elemTop[2]) {
+$(NavElem[2]).addClass('current');
+} 
+}
+
+
+
+
+$('#navi a').click(function () {
+var elmHash = $(this).attr('href'); 
+var headerH = $("#header").outerHeight(true);
+var pos = Math.round($(elmHash).offset().top-headerH); 
+$('body,html').animate({scrollTop: pos}, 700);
+return false;
+});
+
+
+$(window).scroll(function () {
+PositionCheck();
+ScrollAnime();
+});
+
+
+$(window).on('load', function () {
+PositionCheck();
+ScrollAnime();
+});
+
+$(window).resize(function() {
+PositionCheck()
+});
+
+
+
+
 
 $(window).on('load',function(){	
 
